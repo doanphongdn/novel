de19863c3fc9d1f9735836d47ccac1b1475214aa
Đoàn Hữu Thời <thoi.dh@teko.vn>
2021-01-09 03:05:24 +0700

    fix cron uupdate

--- crawl_service/management/commands/get_chapters_content.py
+++ crawl_service/management/commands/get_chapters_content.py
@@ -14,16 +14,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         campaigns = CrawlCampaign.objects.filter(active=True, status='stopped').all()
+
         while True:
             process = CrawlerProcess(get_project_settings())
             campaigns_update = []
             for cam in campaigns:
-                run_able = ((datetime.now() - cam.last_run).total_seconds() / 60) >= cam.repeat_time
-                if run_able:
-                    process.crawl(NovelSpider, campaign=cam)
-                    cam.status = 'running'
-                    cam.save()
-                    campaigns_update.append(cam)
+                # run_able = ((datetime.now() - cam.last_run).total_seconds() / 60) >= cam.repeat_time
+                # if run_able:
+                process.crawl(NovelSpider, campaign=cam)
+                cam.status = 'running'
+                cam.save()
+                # campaigns_update.append(cam)
 
             process.start()  # the script will block here until all crawling jobs are finished
 

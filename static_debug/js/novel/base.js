8a42ae2952e487456ad04a5cd04b0f0f9eb695c5
Đoàn Hữu Thời <thoi.dh@teko.vn>
2021-01-13 03:04:04 +0700

    change base js, remove ui slider

--- static_debug/js/novel/base.js
+++ static_debug/js/novel/base.js
@@ -156,44 +156,4 @@ $(document).ready(function (e) {
         }
     };
     $('.lazyload').Lazy(lazy_param);
-
-    function setInitAttrs() {
-        // Color
-        let textClor = Cookies.get('textColor');
-        let bgColor = Cookies.get('backgroundColor');
-        if (textClor) {
-            setElementsColor(textClor);
-        }
-        if (bgColor) {
-            // Remove class from currently active button
-            $(".colors > li").removeClass("active-color");
-
-            // Add class active to clicked button
-            let currBgColorItem = $(".colors > li[data-color='" + bgColor +"']");
-            $(currBgColorItem).addClass("active-color");
-
-            setBackgroundColor(bgColor);
-        }
-        // Font size
-        let fontSize = Cookies.get('fontSizeSlider');
-        if (fontSize) {
-            fontSizeSlider.noUiSlider.set(fontSize);
-            setChapterFontSize(fontSize);
-        }
-        // Font Weight
-        let fontWeight = Cookies.get('fontWeightSlider');
-        if (fontWeight) {
-            fontWeightSlider.noUiSlider.set(fontWeight);
-            setChapterFontWeight(fontWeight);
-        }
-        // Line Height
-        let lineHeight = Cookies.get('lineHeightSlider');
-        if (lineHeight) {
-            lineHeightSlider.noUiSlider.set(lineHeight);
-            setChapterLineHeight(lineHeight);
-        }
-    }
-
-    setInitAttrs();
-
 });

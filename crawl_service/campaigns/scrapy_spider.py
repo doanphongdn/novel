e52b1c57aeb07964441ed963e25dd52037fa4d0e
Đoàn Hữu Thời <thoi.dh@teko.vn>
2021-01-14 23:51:38 +0700

    fix origin_url

--- crawl_service/campaigns/scrapy_spider.py
+++ crawl_service/campaigns/scrapy_spider.py
@@ -55,8 +55,13 @@ class NovelSpider(scrapy.Spider):
             sleep(self.campaign.paging_delay)
 
         if self.campaign.next_page_xpath and res_data and continue_paging:
-            next_page = response.xpath(self.campaign.next_page_xpath).get()
+            next_page = response.xpath(self.campaign.next_page_xpath).get() or ""
             if origin_url in next_page:
+                if next_page.strip().startswith('//'):
+                    next_page = "http:" + next_page
+                elif next_page.strip().startswith('/'):
+                    next_page = self.campaign.campaign_source.homepage.strip('/') + next_page
+
                 yield scrapy.Request(next_page, cb_kwargs={'origin_url': origin_url, 'paging': True},
                                      callback=self.parse)
         elif self.other_urls:

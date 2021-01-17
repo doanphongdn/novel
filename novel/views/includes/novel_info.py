68fa1f075999c314be4c76c0005f0e48f78b9933
Đoàn Hữu Thời <thoi.dh@teko.vn>
2021-01-18 03:07:04 +0700

    undo than thanh

--- novel/views/includes/novel_info.py
+++ novel/views/includes/novel_info.py
@@ -7,6 +7,28 @@ class NovelInfoTemplateInclude(BaseTemplateInclude):
 
     def __init__(self, include_data, extra_data=None):
         super().__init__(include_data, extra_data)
+
+        comment_enable = self.include_data.get("novel")
+        if comment_enable is None:
+            comment_enable = True
+
+        bookmark_enable = self.include_data.get("novel")
+        if bookmark_enable is None:
+            bookmark_enable = True
+
         self.include_data = {
             "novel": self.include_data.get("novel"),
+            "author_label": self.include_data.get("author_label") or "Authors",
+            "category_label": self.include_data.get("category_label") or "Categories",
+            "latest_update_label": self.include_data.get("latest_update_label") or "Latest update",
+            "chapter_total_label": self.include_data.get("chapter_total_label") or "Chapters",
+            "view_total_label": self.include_data.get("view_total_label") or "Views",
+            "status_label": self.include_data.get("view_total_label") or "Status",
+            "first_chapter_label": self.include_data.get("first_chapter_label") or "First chap",
+            "latest_chapter_label": self.include_data.get("latest_chapter_label") or "Latest chap",
+            "issue_label": self.include_data.get("issue_label") or "Issue",
+            "comment_label": self.include_data.get("comment_label") or "Comments",
+            "bookmark_label": self.include_data.get("bookmark_label") or "Bookmark",
+            "comment_enable": comment_enable,
+            "bookmark_enable": bookmark_enable,
         }

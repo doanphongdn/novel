b102b48efe5a0464b67765d68265c704000d1488
Truc Nguyen <truc.nv@teko.vn>
2021-02-03 03:26:20 +0700

    CDN fix get available cdn

--- crawl_service/models.py
+++ crawl_service/models.py
@@ -119,4 +119,4 @@ class CDNServer(models.Model):
 
     @classmethod
     def get_available_cdn(cls):
-        return cls.objects.filter(active=False, status='stopped').all()
+        return cls.objects.filter(active=True, status='stopped').all()[:1]

5b1ab8bc7da4f341c7a4084086c65e58ee974051
Đoàn Hữu Thời <thoi.dh@teko.vn>
2021-02-04 03:52:07 +0700

    refactor cache

--- novel/cache_manager.py
+++ novel/cache_manager.py
@@ -3,6 +3,4 @@ from cms.cache_manager import CacheManager
 
 class NovelCache(CacheManager):
     def _get_data(self, **kwargs):
-        if hasattr(self.class_model, 'get_available_novel'):
-            raise AttributeError("%s no have function get_available_novel", self.class_model.__name__)
         return self.class_model.get_available_novel().filter(**kwargs).all()

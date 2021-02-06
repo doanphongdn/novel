5c5a20b145ea6101fde396e4dcab37e6c2764003
Truc Nguyen <truc.nv@teko.vn>
2021-02-06 17:12:01 +0700

    fix bug 500 novel view get from cache

--- novel/views/novel.py
+++ novel/views/novel.py
@@ -1,8 +1,9 @@
+import logging
 import re
 from urllib.parse import urlparse
 
 from django.contrib.sites.shortcuts import get_current_site
-from django.http import JsonResponse, StreamingHttpResponse, HttpResponse
+from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
 from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect
@@ -11,6 +12,9 @@ from novel.cache_manager import NovelCache
 from novel.models import Novel
 from novel.views.base import NovelBaseView
 from novel.views.chapter import url2yield
+
+
+logger = logging.getLogger(__name__)
 
 
 class NovelDetailView(NovelBaseView):
@@ -43,50 +47,56 @@ class NovelDetailView(NovelBaseView):
     def get(self, request, *args, **kwargs):
         response = super().get(request, *args, **kwargs)
 
-        chapter_page = request.GET.get('chap-page') or 1
-        slug = kwargs.get('slug')
-        if not slug:
+        try:
+            chapter_page = request.GET.get('chap-page') or 1
+            slug = kwargs.get('slug')
+            if not slug:
+                # TODO: define 404 page
+                # direct to homepage
+                return redirect('home')
+
+            novel = NovelCache().get_from_cache(slug=slug)
+            if novel:
+                referer = urlparse(novel.url)
+                if novel.thumbnail_image.strip().startswith('//'):
+                    referer_url = referer.scheme  # + referer.netloc
+                else:
+                    referer_url = referer.scheme + "://"  # + referer.netloc
+
+                response.context_data["setting"]["title"] = novel.name
+                response.context_data['setting']['meta_img'] = referer_url + novel.thumbnail_image
+                keywords = [novel.slug.replace('-', ' '), novel.name, novel.name + ' full']
+                response.context_data["setting"]["meta_keywords"] += ', ' + ', '.join(keywords)
+
+                breadcrumb_data = [{
+                    "name": novel.name,
+                    "url": novel.get_absolute_url(),
+                }]
+            else:
+                return redirect('home')
+
+            extra_data = {
+                "breadcrumb": {
+                    "breadcrumb_data": breadcrumb_data,
+                },
+                "novel_info": {
+                    "novel": novel,
+                },
+                "chapter_list": {
+                    "novel": novel,
+                    "chap_page": chapter_page,
+                }
+            }
+            response.context_data.update({
+                'include_html': self.incl_manager.render_include_html("novel", extra_data=extra_data),
+                'request_url': request.build_absolute_uri(),
+                'novel': novel,
+            })
+        except Exception as e:
+            logger.error('[NovelDetailView] get view Error %s', e)
             # TODO: define 404 page
             # direct to homepage
             return redirect('home')
-
-        novel = NovelCache().get_from_cache(slug=slug)
-        if novel:
-            referer = urlparse(novel.url)
-            if novel.thumbnail_image.strip().startswith('//'):
-                referer_url = referer.scheme  # + referer.netloc
-            else:
-                referer_url = referer.scheme + "://"  # + referer.netloc
-
-            response.context_data["setting"]["title"] = novel.name
-            response.context_data['setting']['meta_img'] = referer_url + novel.thumbnail_image
-            keywords = [novel.slug.replace('-', ' '), novel.name, novel.name + ' full']
-            response.context_data["setting"]["meta_keywords"] += ', ' + ', '.join(keywords)
-
-            breadcrumb_data = [{
-                "name": novel.name,
-                "url": novel.get_absolute_url(),
-            }]
-        else:
-            return redirect('home')
-
-        extra_data = {
-            "breadcrumb": {
-                "breadcrumb_data": breadcrumb_data,
-            },
-            "novel_info": {
-                "novel": novel,
-            },
-            "chapter_list": {
-                "novel": novel,
-                "chap_page": chapter_page,
-            }
-        }
-        response.context_data.update({
-            'include_html': self.incl_manager.render_include_html("novel", extra_data=extra_data),
-            'request_url': request.build_absolute_uri(),
-            'novel': novel,
-        })
 
         return response
 

3a7934b2623f497c6d9de5837360eff5941272d8
Truc Nguyen <truc.nv@teko.vn>
2021-02-12 17:06:45 +0700

    Update stream

--- novel/views/stream.py
+++ novel/views/stream.py
@@ -52,8 +52,20 @@ def stream_image(request, *args, **kwargs):
             referer_url = json_str.get("referer")
             chapter_id = json_str.get("chapter")
             chapter_updated = json_str.get("chapter_updated")
-            response = StreamingHttpResponse(streaming_content=url2yield(origin_url, referer=referer_url),
-                                             content_type="image/jpeg")
+            try:
+                response = StreamingHttpResponse(streaming_content=url2yield(origin_url, referer=referer_url),
+                                                 content_type="image/jpeg")
+            except Exception as ex:
+                if chapter_id and chapter_updated:
+                    chapter = NovelChapter.objects.get(pk=chapter_id)
+                    if chapter and chapter.chapter_updated:
+                        chapter.chapter_updated = False
+                        chapter.save()
+                print("[stream_image] Error when parse image %s : %s <chapter %s - updated %s>"
+                      % (img_hash, ex, chapter_id, chapter_updated))
+                import traceback
+                traceback.print_exc()
+                return HttpResponse({})
 
             is_stream_failed = False
             stream_content = list(response.streaming_content)

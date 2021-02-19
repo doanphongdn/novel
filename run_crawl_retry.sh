42fca15328b76b13db7c7d78337bb58da1234d8a
Truc Nguyen <truc.nv@teko.vn>
2021-02-20 03:24:08 +0700

    Create bash file to run cron

---
+++ run_crawl_retry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ $(ps -ef | grep "python manage.py run_crawl_retry" | grep -v grep | wc -l) -eq 0 ]; then
+	.venv/bin/python manage.py run_crawl_retry >/dev/null 2>&1 >>/data/cdn/novel/run_crawl_retry.log
+fi

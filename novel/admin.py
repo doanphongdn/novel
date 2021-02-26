364eeca23df36acedad16e3de7aa3e7e6983e7ac
Truc Nguyen <truc.nv@teko.vn>
2021-02-26 21:55:00 +0700

    Update Novel services commands

--- novel/admin.py
+++ novel/admin.py
@@ -6,7 +6,8 @@ from django.contrib import admin
 from django.contrib.admin.views.main import ChangeList
 
 from django_cms.admin import BaseActionAdmin
-from novel.models import CDNNovelFile, Novel, NovelChapter, NovelSetting, Genre
+from django_cms.models import CDNServer
+from novel.models import CDNNovelFile, Genre, Novel, NovelChapter, NovelSetting
 
 
 class NovelForm(forms.ModelForm):
@@ -111,3 +112,8 @@ class NovelSettingAdmin(BaseActionAdmin):
 class CDNNovelFileAdmin(BaseActionAdmin):
     list_display = ("id", "cdn", "chapter", "type", "hash_origin_url", "retry", "full")
     search_fields = ("cdn", "chapter", "hash_origin_url", "url")
+
+
+@admin.register(CDNServer)
+class CDNServerAdmin(BaseActionAdmin):
+    list_display = ("id", "name", "server_id", "endpoint", "last_run", "status")

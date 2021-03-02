66a86bd472029f9b8440c147ae5b9c82a6cb4c15
Đoàn Hữu Thời <thoi.dh@teko.vn>
2021-03-03 02:27:58 +0700

    test old data social

--- custom_allauth/socialaccount/adapter.py
+++ custom_allauth/socialaccount/adapter.py
@@ -1,4 +1,6 @@
+from allauth.account.utils import user_email
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from allauth.socialaccount.models import SocialAccount
 
 from novel.models import NovelUserProfile
 from novel.views.user import UserAction
@@ -7,7 +9,10 @@ from novel.views.user import UserAction
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
 
     def save_user(self, request, sociallogin, form=None):
-        user = super().save_user(request, sociallogin, form)
+        user = SocialAccount.objects.filter(uid=user_email(sociallogin.user)).first()
+        if not user:
+            user = super().save_user(request, sociallogin, form)
+
         NovelUserProfile(user_id=user.id, avatar=sociallogin.account.get_avatar_url()).save()
         return user
 

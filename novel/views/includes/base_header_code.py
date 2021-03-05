8698c55e3f0473862cd6e1a8034a1bb31a37621b
Đoàn Hữu Thời <thoi.dh@teko.vn>
2021-03-05 09:55:48 +0700

    upodate footer

---
+++ novel/views/includes/base_header_code.py
@@ -0,0 +1,17 @@
+from django_cms.utils.cache_manager import CacheManager
+from django_cms.models import FooterInfo
+from novel.views.includes.base import BaseTemplateInclude
+
+
+class FooterInfotemplateInclude(BaseTemplateInclude):
+    name = "base_footer_info"
+    template = "novel/includes/base_footer_info.html"
+
+    def prepare_include_data(self):
+        footer_type = self.include_data.get("footer_type")
+
+        ft_info = CacheManager(FooterInfo, **{"type": footer_type}).get_from_cache(get_all=True)
+
+        self.include_data.update({
+            "content": ft_info and ft_info[0].content or "",
+        })

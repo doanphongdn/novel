d72b99e14f51798f6853f04fb6f990022436c401
Truc Nguyen <truc.nv@teko.vn>
2021-03-11 22:53:22 +0700

    Fix settings

--- novel/settings.py
+++ novel/settings.py
@@ -2,10 +2,11 @@ import os
 
 import redis
 
+
 JSON_LD_DEFAULT_TYPE = 'Book'
 redis_image = redis.StrictRedis(host='localhost', port=6379, db=1)
 
-GOOGLE_RECAPTCHA_SITE_KEY = os.environ.get('GOOGLE_RECAPTCHA_SITE_KEY')
-GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY')
-GOOGLE_ADS = os.environ.get('GOOGLE_ADS')
+GOOGLE_RECAPTCHA_SITE_KEY = os.environ.get('GOOGLE_RECAPTCHA_SITE_KEY', '')
+GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY', '')
+GOOGLE_ADS = os.environ.get('GOOGLE_ADS', '')
 GOOGLE_GPT_ADM_CODE = os.environ.get('GOOGLE_GPT_ADM_CODE', 'True').lower() == 'true'

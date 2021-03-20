848904c5d3f8b2fad282ceebeecf7ca01533337d
Truc Nguyen <truc.nv@teko.vn>
2021-03-20 21:14:25 +0700

    fix number of cron upload cdn thumbnail

--- run_cdn_thumbnails.sh
+++ run_cdn_thumbnails.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ $(ps -ef | grep "python manage.py run_cdn_thumbnails" | grep -v grep | wc -l) -lt 2 ]; then
+if [ $(ps -ef | grep "python manage.py run_cdn_thumbnails" | grep -v grep | wc -l) -eq 0 ]; then
 	.venv/bin/python manage.py run_cdn_thumbnails >/dev/null 2>&1 >>/data/cdn/novel/run_cdn_thumbnails.log
 fi

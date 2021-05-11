700c459c89f3a1faa7fad46556b0296b3942c51b
Doan Phong <thoidh.me@gmail.com>
2021-05-11 13:30:14 +0700

    Merge branch 'new_branch_without_service' into main

--- run_cdn_files.sh
+++ run_cdn_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ $(ps -ef | grep "python manage.py run_cdn_files" | grep -v grep | wc -l) -lt 2 ]; then
-	.venv/bin/python manage.py run_cdn_files >/dev/null 2>&1 >>/data/cdn/novel/run_cdn_files.log
+	.venv/bin/python manage.py run_cdn_files --order_by_list updated_at --multi_thread=2 >/dev/null 2>&1 >>/data/cdn/novel/run_cdn_files.log
 fi

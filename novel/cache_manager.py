1f7e644931bb96ea512745f7761464c3cba63099
Doan Phong <thoidh.me@gmail.com>
2021-05-17 10:16:32 +0700

    fix novel cache

--- novel/cache_manager.py
+++ novel/cache_manager.py
@@ -3,10 +3,13 @@ from django_cms.utils.cache_manager import CacheManager
 
 class NovelCache(CacheManager):
     def _get_data(self, **kwargs):
+        res = self.class_model.get_available_novel().filter(
+            **kwargs).order_by(*self.order_by).prefetch_related("novel_flat").all()
+        
         if self.limit and self.limit > 0:
-            return self.class_model.get_available_novel().filter(
-                **kwargs).prefetch_related("novel_flat").all()[:self.limit]
-        return self.class_model.get_available_novel().filter(**kwargs).prefetch_related("novel_flat").all()
+            return res[:self.limit]
+
+        return res
 
 
 class BookmarkCountCache(CacheManager):

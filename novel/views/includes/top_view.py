fbdc3119fa6f3c34d55809e160c245b5dcf03eff
Doan Phong <thoidh.me@gmail.com>
2021-05-17 10:14:06 +0700

    fix novel cache

--- novel/views/includes/top_view.py
+++ novel/views/includes/top_view.py
@@ -1,5 +1,6 @@
 from django_cms import settings
 from django_cms.utils.cache_manager import CacheManager
+from novel.cache_manager import NovelCache
 from novel.form.auth import LoginForm, RegisterForm, LostPassForm
 from novel.models import Novel
 from novel.views.includes.base import BaseTemplateInclude
@@ -12,9 +13,9 @@ class TopViewTemplateInclude(BaseTemplateInclude):
 
     def prepare_include_data(self):
         super().prepare_include_data()
-        novel_daily = CacheManager(Novel, limit=7, order_by=["view_daily"]).get_from_cache(get_all=True)
-        novel_monthly = CacheManager(Novel, limit=7, order_by=["view_monthly"]).get_from_cache(get_all=True)
-        novel_all = CacheManager(Novel, limit=7, order_by=["view_total"]).get_from_cache(get_all=True)
+        novel_daily = NovelCache(Novel, limit=7, order_by=["view_daily"]).get_from_cache(get_all=True)
+        novel_monthly = NovelCache(Novel, limit=7, order_by=["view_monthly"]).get_from_cache(get_all=True)
+        novel_all = NovelCache(Novel, limit=7, order_by=["view_total"]).get_from_cache(get_all=True)
 
         self.include_data.update({
             "novel_daily": novel_daily,
